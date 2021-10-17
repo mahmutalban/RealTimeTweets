15a19583fa3bdd69c8029de6d07d34ac4db680d5
mahmutalban <mahmut.alban@utb.ecutbildning.se>
2021-10-17 02:37:55 +0200

    added stream rules

--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
-/*http client to make request*/
+// http client to make request
 const needle = require ('needle')
 const config = require ('dotenv').config()
 const TOKEN = process.env.TWITTER_BEARER_TOKEN
 
-/*printing out token*/ 
-console.log(TOKEN)
+// endpoints for the URL's
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id'
+
+// rules that wanted to add
+const rules = [{values: 'giveaway'}]
+
+// get stream rules
+async function getRules () {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`,
+        }
+    })
+
+    console.log(response.body)
+    return response.body
+}
+
+//function that run itself
+;(async () => {
+    let currentRules
+
+    try {
+        currentRules = await getRules()
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}) ()
