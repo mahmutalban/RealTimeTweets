7dd8c1e3decda4a45577e3cf497c1ef2a2d071c7
mahmutalban <mahmut.alban@utb.ecutbildning.se>
2021-10-17 03:04:16 +0200

    added delete rules function

--- server/index.js
+++ server/index.js
@@ -5,28 +5,73 @@ const TOKEN = process.env.TWITTER_BEARER_TOKEN
 
 // endpoints for the URL's
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id'
+const streamURL = 
+'https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id'
 
 // rules that wanted to add
-const rules = [{values: 'giveaway'}]
+const rules = [{value: 'giweaway'}]
 
 // get stream rules
 async function getRules () {
     const response = await needle('get', rulesURL, {
         headers: {
             Authorization: `Bearer ${TOKEN}`,
-        }
+        },
     })
 
     console.log(response.body)
     return response.body
 }
 
-//function that run itself
+// set stream rules
+async function setRules () {
+
+    const data = {
+        add: rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`,
+        },
+    })
+
+    return response.body
+}
+
+// delete stream rules
+async function deleteRules (rules) {
+
+    if(!Array.isArray(rules.data)) {
+        return null
+    }
+
+    const ids = rules.data.map((rule) =>rule.id)
+
+    const data = {
+        delete: {
+            ids: ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`,
+        },
+    })
+
+    return response.body
+}
+
+
+// function that run itself
 ;(async () => {
     let currentRules
 
     try {
+        await setRules()
         currentRules = await getRules()
     } catch (error) {
         console.error(error)
